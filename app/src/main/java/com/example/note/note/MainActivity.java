9c90dab456599457f6e40004e17295be8ac52786
Dark-Existed <c2572388311@gmail.com>
2017-07-21 17:03:28 +0800

    need to modify

--- app/src/main/java/com/example/note/note/MainActivity.java
+++ app/src/main/java/com/example/note/note/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.note.note;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
