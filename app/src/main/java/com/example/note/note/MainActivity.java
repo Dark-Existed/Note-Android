90c5c43f824ec8d9b7c533468ab889231ab50dc1
Dark-Existed <c2572388311@gmail.com>
2017-07-26 16:48:28 +0800

    修改部分id名

--- app/src/main/java/com/example/note/note/MainActivity.java
+++ app/src/main/java/com/example/note/note/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.note.note;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,7 +9,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
@@ -29,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.nav_header, mDrawerLayout, false);
+        ImageView headPortrait = (ImageView) view.findViewById(R.id.headPortrait);
+        headPortrait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InformationActivity.class);
+                startActivity(intent);
+                // TODO: 2017/7/26  无法启动InformationActivity
+            }
+        });
+
 
         navView.setCheckedItem(R.id.nav_note);
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
@@ -60,4 +78,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
