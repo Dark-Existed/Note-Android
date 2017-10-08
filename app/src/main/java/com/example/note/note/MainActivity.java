4f9a08e717a52eb9388762186cc5957fa60ffa19
Dark-Existed <c2572388311@gmail.com>
2017-10-08 16:29:08 +0800

    同步一下

--- app/src/main/java/com/example/note/note/MainActivity.java
+++ app/src/main/java/com/example/note/note/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
